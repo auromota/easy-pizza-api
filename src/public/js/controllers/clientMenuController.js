4c55562d1519f2bc9acfd7718fb0c8133c6db858
Auro <aurofmota@gmail.com>
2016-12-08 15:20:03 -0200

    Set active menu in client dashboard

--- src/public/js/controllers/clientMenuController.js
+++ src/public/js/controllers/clientMenuController.js
@@ -1,5 +1,32 @@
-app.controller('clientMenuController', ['$scope', '$state', 'LoginService', 'Order',
-    function ($scope, $state, LoginService, Order) {
+app.controller('clientMenuController', ['$scope', '$rootScope', '$state', 'LoginService', 'Order',
+    function ($scope, $rootScope, $state, LoginService, Order) {
+
+        processState($state.current.name);
+
+        function processState(stateName) {
+            switch (stateName) {
+                case 'client.orders':
+                    $scope.isOrders = true;
+                    $scope.isDrinks = false;
+                    $scope.isPizzas = false;
+                    break;
+                case 'client.drinks':
+                    $scope.isOrders = false;
+                    $scope.isDrinks = true;
+                    $scope.isPizzas = false;
+                    break;
+                case 'client.pizzas':
+                    $scope.isOrders = false;
+                    $scope.isDrinks = false;
+                    $scope.isPizzas = true;
+                    break;
+                default: break;
+            }
+        }
+
+        $rootScope.$on('$stateChangeStart', function (event, toState) {
+            processState(toState.name);
+        });
 
         $scope.logout = function () {
             LoginService.unregisterClient();
