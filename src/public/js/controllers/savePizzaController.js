85aaeb9de308a35726f8f5c0532b9625eba6cb2d
WellingtonBraga <wellington.jbraga@live.com>
2016-12-08 02:33:24 -0200

    Creating page to insert new Pizza flavor

--- src/public/js/controllers/savePizzaController.js
+++ src/public/js/controllers/savePizzaController.js
@@ -1,3 +1,29 @@
-app.controller('savePizzaController', ['$scope', 'Topping', function($scope, Topping){
-    $scope.toppings = Topping.query();
+app.controller('savePizzaController', ['$scope', 'Topping', function ($scope, Topping) {
+
+    let sortFn = (a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+
+    $scope.toppings = Topping.query(() => $scope.toppings.sort(sortFn));
+
+    $scope.pizza = {};
+    $scope.pizza.toppings = [];
+
+    $scope.addTopping = function (topping) {
+        $scope.pizza.toppings.push(topping);
+
+        $scope.pizza.toppings.sort(sortFn)
+
+        let index = $scope.toppings.indexOf(topping);
+
+        $scope.toppings.splice(index, 1);
+    }
+
+    $scope.removeTopping = function (topping) {
+        $scope.toppings.push(topping);
+        let index = $scope.pizza.toppings.indexOf(topping);
+
+        $scope.toppings.sort(sortFn)
+
+        $scope.pizza.toppings.splice(index, 1);
+    }
+
 }]);
